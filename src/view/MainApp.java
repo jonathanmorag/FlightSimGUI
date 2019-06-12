6e4a273bc1a80bfb8752b802ab446d536580a365
royihamo <royihamo@gmail.com>
2019-06-12 16:57:42 +0300

    100%

--- src/view/MainApp.java
+++ src/view/MainApp.java
@@ -31,7 +31,7 @@ public class MainApp extends Application {
 			FXMLLoader fxl = new FXMLLoader();
 			MainWindowController mwc;
 			BorderPane root = fxl.load(getClass().getResource("MainWindow.fxml").openStream());
-			Scene scene = new Scene(root, 800, 700);
+			Scene scene = new Scene(root, 800, 695);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			mwc = fxl.getController();
 			mwc.setViewModel(vm);
